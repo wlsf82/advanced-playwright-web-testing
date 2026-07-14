a9e7f2f748ae76a72df31699dbf34ae8c37fcc9d
Walmyr <wlsf82@gmail.com>
2026-07-14 20:30:32 +0200

    Add test for empty state
    And adjust the previous one.

--- tests/engageSphere.spec.ts
+++ tests/engageSphere.spec.ts
@@ -3,16 +3,37 @@ import { test, expect } from '@playwright/test';
 import customers from '../mocks/customers.json'
 
 test.describe('EngageSphere', () => {
-  test.beforeEach(async ({ page }) => {
+  test('shows the mocked customer', async ({ page }) => {
     await page.route('**/customers*', async (route) => {
       expect(route.request().method()).toBe('GET')
       await route.fulfill({ json: customers })
     })
-    await page.goto('/')
-  })
 
-  test('shows the mocked customer', async ({ page }) => {
+    await page.goto('/')
+
     await expect(page.getByRole('table')).toBeVisible()
     await expect(page.getByText('SpaceY Inc.')).toBeVisible()
+  })
+
+  test('shows the empty state when there are no customers', async ({ page }) => {
+    await page.route('**/customers*', async (route) => {
+      await route.fulfill({
+        json: {
+          customers: [],
+          pageInfo: {
+            currentPage: 1,
+            totalPages: 1,
+            totalCustomers: 0
+          }
+        },
+      })
+    })
+
+    await page.goto('/')
+
+    await expect(page.locator('svg[title="image of an empty box"]')).toBeVisible()
+    await expect(page.getByText('No customers available.')).toBeVisible()
+    await expect(page.getByTestId('name')).toBeDisabled()
+    await expect(page.getByRole('button', { name: 'Download CSV' })).toHaveCount(0)
   })
 })
