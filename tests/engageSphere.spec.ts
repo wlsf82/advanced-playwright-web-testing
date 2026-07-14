b17848b8ee1b6b72dd19cdf8f6b9f751be39502c
Walmyr <wlsf82@gmail.com>
2026-07-14 19:57:42 +0200

    Update test to use mock data instead

--- tests/engageSphere.spec.ts
+++ tests/engageSphere.spec.ts
@@ -1,16 +1,18 @@
 import { test, expect } from '@playwright/test';
 
+import customers from '../mocks/customers.json'
+
 test.describe('EngageSphere', () => {
   test.beforeEach(async ({ page }) => {
     await page.route('**/customers*', async (route) => {
-      console.log('Request URL:', route.request().url())
       expect(route.request().method()).toBe('GET')
-      await route.continue()
+      await route.fulfill({ json: customers })
     })
     await page.goto('/')
   })
 
-  test('loads the customers table', async ({ page }) => {
+  test('shows the mocked customer', async ({ page }) => {
     await expect(page.getByRole('table')).toBeVisible()
+    await expect(page.getByText('SpaceY Inc.')).toBeVisible()
   })
 })
